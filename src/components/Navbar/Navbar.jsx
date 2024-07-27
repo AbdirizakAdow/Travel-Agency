b1349be72dcdadb104003054cb64915aad211edf
abdirizakadow506@gmail.come~ <Abdirizak Adow>
2024-07-27 23:23:35 +0300

    hi

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-    <dev>
-      <dev className="container"></dev>
+    <dev className="fixed top-0 right-0 w-full bg-white text-black shadow-md">
+      <dev className="container bg-gradiemt-to-r from-primary">
+        <dev className = "flex">
+          <p>20% off on next booking</p>
+          <p>Mobile No. +252 612847462</p>
+        </dev>
+      </dev>
     </dev>
     </>
   )
