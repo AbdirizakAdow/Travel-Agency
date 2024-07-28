3b172041329467e52eb15a84dc04b03ff34096d7
abdirizakadow506@gmail.come~ <Abdirizak Adow>
2024-07-28 03:33:12 +0300

    midde night commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import LogoIm from "../../assets/logo.png";
+import { FaCaretDown } from 'react-icons/fa';
 
+const DropdownLinks = [
+  {
+    name: "Our Services",
+    link: "/services",
+  },
+  {
+    name: "Top Brands",
+    link: "/mobile_brands",
+  },
+  {
+    name: "Location",
+    link: "/location",
+  },
+]
 const Navbar = () => {
   return (
     <>
@@ -18,9 +33,66 @@ const Navbar = () => {
         <div className="flex justify-between items-center ">
           {/* logo section */}
           <div>
-            <Link to="/">
-            < img src={LogoIm} alt=""/>
+            <Link to="/" activeClassName="active" onClick={() => 
+              window.scrollTo(0, 0)}>
+            < img src={LogoIm} alt="" className='h-16'/>
             </Link>
+            {/* Desktop Navlink section */}
+          </div>
+          <div className="hidden md:block">
+            <ul className=" flex items-center gap-6">
+              <li className="py-4">
+                <NavLink activeClassName="active" to= "/" onClick={() => window.
+                  scrollTo(0, 0)}>
+                    Home
+                </NavLink>
+              </li>
+              <li className="py-4">
+                <NavLink activeClassName="active" to= "/blogs" onClick={() => window.
+                  scrollTo(0, 0)}>
+                    Blogs
+                </NavLink>
+              </li>
+              <li className="py-4">
+                <NavLink activeClassName="active" to= "/places" onClick={() => window.
+                  scrollTo(0, 0)}>
+                    Best places
+                </NavLink>
+              </li>
+              <li className="py-4">
+                <NavLink activeClassName="active" to= "/about" onClick={() => window.
+                  scrollTo(0, 0)}>
+                    About
+                </NavLink>
+              </li>
+              {/* Dropdown section */}
+              <li className="py-4 relative group cursor-pointer">
+                <div className="dropdown  flex items-center">
+                  <span>Quick links</span>
+                  <span>
+                    <FaCaretDown className="transition-all duration-200 group-hover:rotate-180:"></FaCaretDown>
+                  </span>
+                </div>
+                <dir className="absolute -left-9 top-
+                [57px] z-[9999] hidden group-hover:block 
+                shadow-md text-black w-[150px] bg-white p-2">
+                  <ul>
+                    { DropdownLinks.map((data) => {
+                      return (
+                        <li key={data.name}>
+                          <a className="inline-block 
+                          w-full rounded-md p-2 
+                          hover:bg-primary/20"
+                          href={data.link}>
+                            {data.link}
+                          </a>
+                        </li>
+                      );
+                      })};
+                  </ul>
+                </dir>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
