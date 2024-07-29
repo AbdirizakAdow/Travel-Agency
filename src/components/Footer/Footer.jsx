a3f6e4e914d1509f7cc3eaa449bece82bc2d211a
abdirizakadow506@gmail.come~ <Abdirizak Adow>
2024-07-29 06:23:45 +0300

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -60,7 +60,7 @@ const Footer = () => {
               </div>
               <div className="flex items-center gap-3 mt-3">
                 <FaMobileAlt />
-                <p>+91 123456789</p>
+                <p>+252612847462</p>
               </div>
               {/* social handles */}
               <div>
